c020b9824e8aa7371b9da4439de234d544b0b174
jisng <jxsxxng@gmail.com>
2020-01-14 19:56:09 +0900

    create

---
+++ HousekeepingBook/HousekeepingBook/SceneDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  SceneDelegate.swift
+//  HousekeepingBook
+//
+//  Created by 박지승 on 2020/01/13.
+//  Copyright © 2020 Jisng. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let dayVC = DayViewController()
+        dayVC.tabBarItem = UITabBarItem(title: "Today", image: nil
+            , tag: 0)
+        let monthVC = MonthViewController()
+        monthVC.tabBarItem = UITabBarItem(title: "Month", image: nil
+        , tag: 1)
+        let statsVC = StatsViewController()
+        statsVC.tabBarItem = UITabBarItem(title: "Stats", image: nil
+        , tag: 2)
+        
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [dayVC, monthVC, statsVC]
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = tabBarController
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
---
+++ HousekeepingBook/HousekeepingBook/StatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatsViewController.swift
+//  HousekeepingBook
+//
+//  Created by 박지승 on 2020/01/13.
+//  Copyright © 2020 Jisng. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
